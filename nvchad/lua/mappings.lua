eaa8624165391a10db90377e173b33757e707e92
Jackson Breit <jackson.breit@outlook.com>
2024-09-27 09:57:51 -0700

    Remove some annoying nvim bindings

--- nvchad/lua/mappings.lua
+++ nvchad/lua/mappings.lua
@@ -6,10 +6,4 @@ local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 
--- Map superfluous actions to <Esc>
-map("i", "jk", "<ESC>")
-map("i", "kj", "<ESC>")
-map("i", "hl", "<ESC>")
-map("i", "lh", "<ESC>")
-
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")

abaf8917464a9d684682de0bf1b872e2fcbd335b
Jackson Breit <84806918+NinjacksonXV@users.noreply.github.com>
2024-09-28 16:11:08 -0700

    Add pyright

--- nvchad/lua/configs/lspconfig.lua
+++ nvchad/lua/configs/lspconfig.lua
@@ -4,7 +4,7 @@ local on_init = require("nvchad.configs.lspconfig").on_init
 local capabilities = require("nvchad.configs.lspconfig").capabilities
 
 local lspconfig = require "lspconfig"
-local servers = { "html", "cssls" , "clangd", "lua_ls", "nil_ls", "ts_ls", "bashls", "gopls"}
+local servers = { "html", "cssls" , "clangd", "lua_ls", "nil_ls", "ts_ls", "bashls", "gopls", "pyright"}
 
 -- lsps with default config
 for _, lsp in ipairs(servers) do

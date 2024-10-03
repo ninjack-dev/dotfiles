4ffe46bd2b006fe2c9729b941586a8c4bc7aa8a8
Jackson Breit <84806918+NinjacksonXV@users.noreply.github.com>
2024-10-03 14:00:23 -0700

    Hyprland additions

--- hypr/screenshot.sh
+++ hypr/screenshot.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
 # Disable fading during layer exit
+CAPTURE_PATH=$HOME/Pictures/Captures/"$(date +%m-%y)"
+mkdir -p $CAPTURE_PATH
 hyprctl keyword animation "fadeLayersOut, 0"  
 # The following logic prevents hanging. -g - reads from STDIN, and errors out if it's empty (which it is if slurp fails)
-slurp -d | grim -g - - | tee $HOME/Pictures/Captures/$(date +%m-%y)/$(date +%d-%H:%M).png | $COPY_UTIL
+slurp -d | grim -g - - | tee $CAPTURE_PATH/$(date +%d-%H:%M).png | $COPY_UTIL
 # Re-enable fading during layer exit (no use for it yet but we'll see)
 hyprctl keyword animation "fadeLayersOut, 1"

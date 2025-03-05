c8adc157d7d871552dcdb171908bacc9335f5f1b
Jackson Breit <84806918+NinjacksonXV@users.noreply.github.com>
2025-03-05 09:34:21 -0800

    Add nix fmt to Nil LS

--- nvchad/lua/configs/lspconfig.lua
+++ nvchad/lua/configs/lspconfig.lua
@@ -3,8 +3,8 @@ local on_init = require("nvchad.configs.lspconfig").on_init
 local capabilities = require("nvchad.configs.lspconfig").capabilities
 
 local lspconfig = require "lspconfig"
-local servers = { "html", "cssls", "clangd", "nil_ls", "ts_ls", "bashls", "gopls", "pyright", "taplo",
-  "arduino_language_server", "csharp_ls", }
+local servers = { "html", "cssls", "clangd", "ts_ls", "bashls", "gopls", "pyright", "taplo",
+  "arduino_language_server", "csharp_ls", "openscad_lsp", }
 
 -- lsps with default config
 for _, lsp in ipairs(servers) do
@@ -20,4 +20,17 @@ lspconfig.millet.setup {
   on_init = on_init,
   capabilities = capabilities,
   cmd = { 'millet-ls' },
+}
+
+lspconfig.nil_ls.setup  {
+  on_attach = on_attach,
+  on_init = on_init,
+  capabilities = capabilities,
+  settings = {
+    ['nil'] = {
+      formatting = {
+        command = { "nix fmt" },
+      },
+    },
+  },
 }

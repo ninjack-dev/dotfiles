601bf9420824959b32ab239e32423ae5285a5290
Jackson Breit <84806918+NinjacksonXV@users.noreply.github.com>
2025-04-18 11:22:53 -0700

    Format flake.nix

--- ags/flake.nix
+++ ags/flake.nix
@@ -10,39 +10,47 @@
     };
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-    ags,
-    }: let
+  outputs =
+    {
+      self,
+      nixpkgs,
+      ags,
+    }:
+    let
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
-    in {
-      packages.${system}.default = 
-        pkgs.buildEnv { 
-          name = "ags"; 
-          paths = with pkgs; builtins.attrValues (
+    in
+    {
+      packages.${system}.default = pkgs.buildEnv {
+        name = "ags";
+        paths =
+          with pkgs;
+          builtins.attrValues (
             # This grabs all Astal packages from the AGS flake. Might be a more efficient way of doing it.
-            builtins.removeAttrs ags.packages.${system} ["default" "ags" "docs" ] 
-          ) ++ [
-              fzf
-              adwaita-icon-theme
-              # (ags.packages.${system}.agsFull.override {
-              #   extraPackages = with pkgs; [
-              #     Extra packages for AGS runtime here
-              #   ];
-              # })
-            ];
-        }; 
+            builtins.removeAttrs ags.packages.${system} [
+              "default"
+              "ags"
+              "docs"
+            ]
+          )
+          ++ [
+            fzf
+            adwaita-icon-theme
+            # (ags.packages.${system}.agsFull.override {
+            #   extraPackages = with pkgs; [
+            #     Extra packages for AGS runtime here
+            #   ];
+            # })
+          ];
+      };
     };
 }
-
 
 # Dev shell in case I decide to remove AGS/Astall binaries from PATH. Unlikely
 # devShells.${system} = {
 #   default = pkgs.mkShell {
 #
-#     packages = [ 
+#     packages = [
 #       ags.packages.${system}.agsFull
 #     ];
 #
@@ -61,19 +69,19 @@
 
 #       # Packages listed here for later convenience
 #       # ags.packages.${system}.io
-#       # ags.packages.${system}.astal3 
+#       # ags.packages.${system}.astal3
 #       # ags.packages.${system}.astal4
-#       # ags.packages.${system}.apps 
+#       # ags.packages.${system}.apps
 #       # ags.packages.${system}.auth
-#       # ags.packages.${system}.battery 
-#       # ags.packages.${system}.bluetooth 
-#       # ags.packages.${system}.cava # This is broken for now. 
-#       # ags.packages.${system}.greet 
-#       # ags.packages.${system}.hyprland 
-#       # ags.packages.${system}.mpris 
-#       # ags.packages.${system}.network 
-#       # ags.packages.${system}.notifd 
-#       # ags.packages.${system}.powerprofiles 
-#       # ags.packages.${system}.river 
-#       # ags.packages.${system}.tray 
-#       # ags.packages.${system}.wireplumber 
+#       # ags.packages.${system}.battery
+#       # ags.packages.${system}.bluetooth
+#       # ags.packages.${system}.cava # This is broken for now.
+#       # ags.packages.${system}.greet
+#       # ags.packages.${system}.hyprland
+#       # ags.packages.${system}.mpris
+#       # ags.packages.${system}.network
+#       # ags.packages.${system}.notifd
+#       # ags.packages.${system}.powerprofiles
+#       # ags.packages.${system}.river
+#       # ags.packages.${system}.tray
+#       # ags.packages.${system}.wireplumber

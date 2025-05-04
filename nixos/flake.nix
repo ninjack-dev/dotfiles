2a826fa25cd548cb52acb1adf57151fa21bdae79
Jackson Breit <84806918+NinjacksonXV@users.noreply.github.com>
2025-05-04 08:35:52 -0700

    Disable channels and point NIX_PATH at flake outputs

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -18,10 +18,8 @@
     let
       system = "x86_64-linux";
       overlay-unstable = final: prev: {
-        # https://discourse.nixos.org/t/allow-unfree-in-flakes/29904/2
-        # TODO: Figure out how to get the prev.system in here.
         unstable = import nixpkgs-unstable {
-          system = "x86_64-linux";
+          inherit system;
           config.allowUnfree = true;
         };
       };
@@ -30,21 +28,18 @@
       };
     in
     {
-      # As nice as using `nix fmt` would be, for now, it doesn't make much sense to use, as it's really a flake only feature. 
-      # If I have any generic .nix file anywhere in my system, I can either `nix fmt <relative path>` while in `.config/nixos`,
-      # or I can run `nix run <path to flake dir>#formatter.x86_64-linux <file>`, which is immensely tedious. 
-      # Best to just use nixfmt via nixpkgs.
-      # formatter.x86_64-linux = nixpkgs.legacyPackages.x86_64-linux.nixfmt-rfc-style;
       nixosConfigurations."nixos-laptop" = nixpkgs.lib.nixosSystem {
         inherit system;
-        specialArgs = { inherit inputs; };
+        specialArgs = { inherit inputs nixpkgs nixpkgs-unstable; };
         modules = [
           (
             { config, pkgs, ... }:
             {
               nix = {
+                channel.enable = false;
                 registry = {
                   nixpkgs.flake = nixpkgs;
+                  nixpkgs-unstable.flake = nixpkgs-unstable;
                   nixos-hardware.flake = nixos-hardware;
                 };
               };
@@ -52,6 +47,11 @@
               nixpkgs.overlays = [
                 overlay-stable
                 overlay-unstable
+              ];
+
+              nix.nixPath = [
+                "nixpkgs=${nixpkgs.outPath}"
+                "unstable=${nixpkgs-unstable.outPath}"
               ];
             }
           )

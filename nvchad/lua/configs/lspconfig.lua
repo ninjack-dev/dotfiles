02979ec774681ff68d608f6adcceee7214550ec8
Jackson Breit <84806918+NinjacksonXV@users.noreply.github.com>
2025-05-09 08:07:38 -0700

    Fix Nix LS

--- nvchad/lua/configs/lspconfig.lua
+++ nvchad/lua/configs/lspconfig.lua
@@ -1,11 +1,11 @@
 require("nvchad.configs.lspconfig").defaults()
 
 local servers = { "html", "cssls", "clangd", "ts_ls", "bashls", "gopls", "pyright",
-  "arduino_language_server", "csharp_ls", "openscad_lsp", 'nil', 'vala_ls' }
+  "arduino_language_server", "csharp_ls", "openscad_lsp", 'nil_ls', 'vala_ls', 'taplo' }
 
 vim.lsp.enable(servers)
 
-vim.lsp.config('nil', {
+vim.lsp.config('nil_ls', {
   settings = {
     ['nil'] = {
       formatting = {
@@ -19,8 +19,6 @@ vim.lsp.config('vala_ls', {
   single_file_support = true,
 })
 
-
-vim.lsp.enable('taplo')
 vim.lsp.config('taplo', {
   root_markers = { "taplo.toml", ".taplo.toml", ".git" }, -- Waiting for https://github.com/neovim/nvim-lspconfig/pull/3145
 })

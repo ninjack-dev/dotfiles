aa8de51f6b04330b4dd62bb62500660be50e8971
Jackson Breit <84806918+NinjacksonXV@users.noreply.github.com>
2025-05-26 16:17:20 -0700

    Remove colon mapping

--- nvchad/lua/mappings.lua
+++ nvchad/lua/mappings.lua
@@ -4,7 +4,6 @@ require "nvchad.mappings"
 
 local map = vim.keymap.set
 
-map("n", ";", ":", { desc = "CMD enter command mode" })
 map('i', '<C-BS>', '<C-o>db', { noremap = true, silent = true })
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")

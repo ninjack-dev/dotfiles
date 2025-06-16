def10610356a4980186cde8d65a4243f0b1830a8
Jackson Breit <84806918+NinjacksonXV@users.noreply.github.com>
2025-06-16 15:16:23 -0700

    Add yamlls and replace taplo

--- nvchad/lua/configs/lspconfig.lua
+++ nvchad/lua/configs/lspconfig.lua
@@ -1,11 +1,11 @@
 require("nvchad.configs.lspconfig").defaults()
 
 local servers = { "html", "cssls", "clangd", "ts_ls", "bashls", "gopls", "pyright",
-  "arduino_language_server", "csharp_ls", "openscad_lsp", 'nil_ls', 'vala_ls', 'taplo', 'gdscript' }
+  "arduino_language_server", "csharp_ls", "openscad_lsp", "nil_ls", "vala_ls", "tombi", "gdscript", "yamlls" }
 
-vim.lsp.config('nil_ls', {
+vim.lsp.config("nil_ls", {
   settings = {
-    ['nil'] = {
+    ["nil"] = {
       formatting = {
         command = { "nixfmt" },
       },
@@ -13,12 +13,20 @@ vim.lsp.config('nil_ls', {
   },
 })
 
-vim.lsp.config('vala_ls', {
-  single_file_support = true,
+vim.lsp.config("yamlls", {
+  settings = {
+    yaml = {
+      format = { enable = true },
+      schemas = {
+        ["https://json.schemastore.org/github-workflow.json"] = "/.github/workflows/*",
+        ["https://raw.githubusercontent.com/compose-spec/compose-spec/master/schema/compose-spec.json"] = { "*compose*.y*ml" },
+      }
+    }
+  }
 })
 
-vim.lsp.config('taplo', {
-  root_markers = { "taplo.toml", ".taplo.toml", ".git" }, -- Waiting for https://github.com/neovim/nvim-lspconfig/pull/3145
+vim.lsp.config("vala_ls", {
+  single_file_support = true,
 })
 
 vim.lsp.enable(servers)

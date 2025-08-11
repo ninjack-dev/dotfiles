fe53acd0e8dc0d98e94e0f549425b93c59f375be
Jackson Breit <84806918+ninjack-dev@users.noreply.github.com>
2025-08-10 19:08:20 -0700

    Add window resize bind

--- nvchad/lua/mappings.lua
+++ nvchad/lua/mappings.lua
@@ -1,9 +1,17 @@
 require "nvchad.mappings"
 
--- add yours here
-
 local map = vim.keymap.set
 
-map("i", "<C-BS>", "<C-w>", { noremap = true, silent = true, desc = "Delete backwards by a word (same as <C-w>)"})
+map("i", "<C-BS>", "<C-w>", { noremap = true, silent = true, desc = "Delete backwards by a word (same as <C-w>)" })
 
--- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+-- See :help <Tab>
+map("n", "<C-m>", "<C-m>")
+map("n", "<C-i>", "<C-i>")
+
+map("n", "<C-w><C-m>", function()
+  vim.cmd([[
+    let longest = max(map(range(1, line('$')), "virtcol([v:val, '$'])"))
+    exec "vertical resize " . (longest + 4)
+  ]])
+end, { silent = true, desc = "Shrink window to size of longest line."}
+)

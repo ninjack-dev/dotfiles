e8f652b8e3043c4b845747de632a54857d1dc01f
Jackson Breit <84806918+ninjack-dev@users.noreply.github.com>
2025-09-10 13:22:27 -0700

    fix(hyprland): compare against result of confirmation for window screenshare properly

--- hypr/scripts/toggle_noscreenshare.sh
+++ hypr/scripts/toggle_noscreenshare.sh
@@ -4,7 +4,7 @@ activewindow_address=$(hyprctl -j activewindow | jq -r '.address')
 
 if [[ $(hyprctl getprop "address:$activewindow_address" noscreenshare) == "true" ]]; then
   action=$(notify-send --icon=hyprland --app-name='Hyprland' "The current window is currently blocked from screenshare. Are you sure you want to reveal it?" --action=Yes --action='No (default)' 2> /dev/null)
-  if [[ $action == "Yes" ]]; then
+  if [[ $action == "0" ]]; then
     hyprctl dispatch setprop "address:$activewindow_address" noscreenshare off
   fi
 else

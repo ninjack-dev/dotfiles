5450ab6f2a80bd56b01f613f1d0e789c977614f4
Jackson Breit <84806918+ninjack-dev@users.noreply.github.com>
2025-10-02 08:22:04 -0700

    feat(nixos): add flatub beta repo to flatpak service

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -83,6 +83,7 @@ in
     path = [ pkgs.flatpak ];
     script = ''
       flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+      flatpak remote-add --if-not-exists flathub-beta https://flathub.org/beta-repo/flathub-beta.flatpakrepo 
     '';
   };
 

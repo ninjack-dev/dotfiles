16c811384480c1a6795afb7fcf614fd3c13435d0
Jackson Breit <84806918+ninjack-dev@users.noreply.github.com>
2026-03-12 11:36:24 -0700

    refactor(hypr): update launch-goto-or-pull.sh to allow split program names and title

--- hypr/scripts/launch-goto-or-pull.sh
+++ hypr/scripts/launch-goto-or-pull.sh
@@ -1,10 +1,57 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
-if hyprctl clients | grep 'class: '"$1"; then
-  if [ "$2" = 'pull' ]; then
-hyprctl dispatch movetoworkspace e+0, 'class:'"$1"
-  fi
-    hyprctl dispatch focuswindow 'class:'"$1"
-else 
-  $1
+# Usage: <program to launch> <window class> <action>
+class=
+program=
+action=
+
+while [[ "$#" -gt 0 ]]; do
+  [[ $1 == --*=* ]] && set -- "${1%%=*}" "${1#*=}" "${@:2}"
+  # Mostly unusable for now
+  case $1 in
+    --class*)
+      class="$2"
+      shift 1 2> /dev/null || {
+        printf "No class provided with %s!\n" "$1" >&2
+        exit 1
+      }
+      ;;
+    --program*)
+      program="$2"
+      shift 1 2> /dev/null || {
+        printf "No program provided with %s!\n" "$1" >&2
+        exit 1
+      }
+      ;;
+    --action*)
+      action="$2"
+      shift 1 2> /dev/null || {
+        printf "No action provided with %s!\n" "$1" >&2
+        exit 1
+      }
+      ;;
+    *)
+      if [[ -z "$program" ]]; then
+        program=$1
+      elif [[ -z "$class" ]]; then
+        class=$1
+      elif [[ -z "$action" ]]; then
+        action=$1
+      fi
+      ;;
+  esac
+  shift 1 2> /dev/null
+done
+
+if hyprctl clients | grep "class: $class" > /dev/null 2>&1; then
+  case "$action" in
+    "goto")
+      hyprctl dispatch focuswindow "class:$class"
+      ;;
+    "pull")
+      hyprctl dispatch movetoworkspace e+0, "class:$class"
+      ;;
+  esac
+else
+  hyprctl dispatch exec "$program"
 fi

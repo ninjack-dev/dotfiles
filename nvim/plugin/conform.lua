186cb0be085234abaf8a182dad32243a81db63b2
ninjack-dev <84806918+ninjack-dev@users.noreply.github.com>
2026-05-07 11:48:06 -0700

    feat(nvim): use prettier for markdown formatting

--- nvim/plugin/conform.lua
+++ nvim/plugin/conform.lua
@@ -5,6 +5,7 @@ vim.pack.add({
 require("conform").setup({
   formatters_by_ft = {
     lua = { "stylua" },
+    markdown = { "prettier" },
     python = { "black" },
     css = { "prettier" },
     html = { "prettier" },

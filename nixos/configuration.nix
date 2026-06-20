194cf181dd306a6de039938cea0be4e13fd87824
ninjack-dev <84806918+ninjack-dev@users.noreply.github.com>
2026-06-20 12:11:24 -0700

    fix(nixos): flatpak repo updater should no longer break on rebuild

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -100,15 +100,24 @@ in
   };
 
   services.flatpak.enable = true;
-  # systemd.services.flatpak-repo = {
-  #   wantedBy = [ "multi-user.target" ];
-  #   wants = [ "network-online.target" ];
-  #   path = [ pkgs.flatpak ];
-  #   script = ''
-  #     flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-  #     flatpak remote-add --if-not-exists flathub-beta https://flathub.org/beta-repo/flathub-beta.flatpakrepo
-  #   '';
-  # };
+  systemd.services.flatpak-repo = {
+    wantedBy = [ "multi-user.target" ];
+    wants = [ "network-online.target" ];
+    after = [ "network-online.target" ];
+    path = [ pkgs.flatpak ];
+    serviceConfig = {
+      Type = "oneshot";
+      RemainAfterExit = true;
+      Restart = "on-failure";
+      RestartSec = "10s";
+      TimeoutStartSec = "5min";
+    };
+    restartIfChanged = false;
+    script = ''
+      flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+      flatpak remote-add --if-not-exists flathub-beta https://flathub.org/beta-repo/flathub-beta.flatpakrepo
+    '';
+  };
 
   networking.firewall = {
     enable = true;

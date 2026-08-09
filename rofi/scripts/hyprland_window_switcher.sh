c5bc36f085b3ddd3874de1d1794fbdcb678fb6ec
ninjack-dev <84806918+ninjack-dev@users.noreply.github.com>
2026-08-09 14:24:00 -0700

    refactor(rofi): update Hyprland window switcher for 0.55

--- rofi/scripts/hyprland_window_switcher.sh
+++ rofi/scripts/hyprland_window_switcher.sh
@@ -3,14 +3,13 @@
 
 declare -r INITIAL_CALL=0
 declare -r DEFAULT_SELECT=1
-declare -r CUSTOM_ENTRY=2
 declare -r CUSTOM_BIND_1=10
 
 if [ "$ROFI_RETV" -eq $INITIAL_CALL ]; then
   echo -en "\0prompt\x1f\n"
   echo -en "\0use-hot-keys\x1ftrue\n"
-  hyprctl clients | 
-  awk '
+  hyprctl clients \
+    | awk '
     BEGIN { ORS = "\n" }
     /Window/ { window_id = $2 }
     /class:/ { class = substr($0, index($0,$2)) }
@@ -23,29 +22,25 @@ if [ "$ROFI_RETV" -eq $INITIAL_CALL ]; then
     # print window_id ", " class ", \"" title "\", " focusHistoryID
     # print window_id "\n" class "\n" title "\n" focusHistoryID "\n" 
     }
-    ' |
-  sort -t$'\t' -k4n | { # I hope windows can't have a tab in the title
-  while IFS=$'\t' read -r window_id class title focus_index; do
-    if [ "$focus_index" -eq 0 ]; then
-      # Don't include the currently focused window
-      continue
-    fi
-    # Encode the window address in the `info` category, referenced by ROFI_INFO
-    echo -en "$title\0icon\x1f$class\x1finfo\x1f$window_id\n"; 
-  done 
+    ' \
+    | sort -t$'\t' -k4n | { # I hope windows can't have a tab in the title
+    while IFS=$'\t' read -r window_id class title focus_index; do
+      if [ "$focus_index" -eq 0 ]; then
+        # Don't include the currently focused window
+        continue
+      fi
+      # Encode the window address in the `info` category, referenced by ROFI_INFO
+      echo -en "$title\0icon\x1f$class\x1finfo\x1f$window_id\n"
+    done
   }
   exit 0
 fi
 
 case "$ROFI_RETV" in
-  $DEFAULT_SELECT)
-  coproc hyprctl dispatch focuswindow address:"0x$ROFI_INFO" >/dev/null 2>&1 
-  # For some reason, without coproc, the dispatcher doesn't follow through all the way; it moves the cursor but doesn't focus the window
+  "$DEFAULT_SELECT")
+    hyprctl dispatch "hl.dsp.focus({ window = \"address:0x$ROFI_INFO\" })" > /dev/null 2>&1
     ;;
-  $CUSTOM_BIND_1)
- coproc hyprctl dispatch movetoworkspace \
-   "$(hyprctl monitors | awk '/active workspace:/ { workspace = $3} /focused: yes/ { print workspace }')",\
-    address:"0x$ROFI_INFO" >/dev/null 2>&1
+  "$CUSTOM_BIND_1")
+    hyprctl dispatch "hl.dsp.window.move({ workspace = \"$(hyprctl monitors | awk '/active workspace:/ { workspace = $3} /focused: yes/ { print workspace }')\", window = \"address:0x$ROFI_INFO\" })" > /dev/null 2>&1
     ;;
 esac
-
